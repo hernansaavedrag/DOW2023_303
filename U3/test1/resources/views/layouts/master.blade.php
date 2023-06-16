7ece6237b7c3334a2a94b08e8e129b2655bfcaab
hernansaavedrag <hernan.saavedra@gmail.com>
2023-06-16 09:26:21 -0400

    trabaando con login

--- U3/test1/resources/views/layouts/master.blade.php
+++ U3/test1/resources/views/layouts/master.blade.php
@@ -43,8 +43,8 @@
                             <a class="nav-link @if(Route::current()->getName()=='home.index') active @endif" aria-current="page" href="{{route('home.index')}}">Inicio</a>
                         </li>
                         <li class="nav-item">
-                            {{-- <a class="nav-link @if(Route::current()->getName()=='equipos.index') active @endif" href="{{route('equipos.index')}}">Equipos</a> --}}
-                            <a class="nav-link @if(Request::segments()[0]=='equipos') active @endif" href="{{route('equipos.index')}}">Equipos</a>
+                            <a class="nav-link @if(Route::current()->getName()=='equipos.index') active @endif" href="{{route('equipos.index')}}">Equipos</a>
+                            {{--<a class="nav-link @if(Request::segments()[0]=='equipos') active @endif" href="{{route('equipos.index')}}">Equipos</a>--}}
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Estadios</a>
@@ -53,8 +53,8 @@
                             <a class="nav-link" href="#">Estadísticas</a>
                         </li>
                         <li class="nav-item">
-                            {{-- <a class="nav-link @if(Route::current()->getName()=='jugadores.index') active @endif" href="{{route('jugadores.index')}}">Jugadores</a> --}}
-                            <a class="nav-link @if(Request::segments()[0]=='jugadores') active @endif" href="{{route('jugadores.index')}}">Jugadores</a>
+                            <a class="nav-link @if(Route::current()->getName()=='jugadores.index') active @endif" href="{{route('jugadores.index')}}">Jugadores</a>
+                            {{--<a class="nav-link @if(Request::segments()[0]=='jugadores') active @endif" href="{{route('jugadores.index')}}">Jugadores</a>--}}
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Partidos</a>
